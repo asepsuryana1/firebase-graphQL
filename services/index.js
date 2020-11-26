044d3bb7820ad8a9d2204aaeddd5f9402f0ef881
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-26 15:45:54 +0700

    copy crud get from router

--- services/index.js
+++ services/index.js
@@ -1,5 +1,14 @@
 const firebase = require("firebase");
 
 const getUsers = () => {
-    // fields of crud 
+    const userReference = firebase.database().ref("/Users/");
+    //Attach an asynchronous callback to read the data
+    userReference.on("value", function(snapshot) {
+      console.log(snapshot.val());
+      res.json(snapshot.val());
+      userReference.off("value");
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+      res.send("The read failed: " + errorObject.code);
+    });
 }
