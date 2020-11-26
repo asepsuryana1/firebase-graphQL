50a0403115b59c4ce4a1e6096eefa64545bbfee2
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-26 15:52:38 +0700

    return promise baSE resolve, reject   must be in promise base

--- services/index.js
+++ services/index.js
@@ -3,12 +3,14 @@ const firebase = require("firebase");
 const getUsers = () => {
     const userReference = firebase.database().ref("/Users/");
     //Attach an asynchronous callback to read the data
-    userReference.on("value", function(snapshot) {
-      console.log(snapshot.val());
-      res.json(snapshot.val());
-      userReference.off("value");
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-      res.send("The read failed: " + errorObject.code);
-    });
+    return (new Promise((resolve, reject) => {
+        userReference.on("value", function (snapshot) {
+            console.log(snapshot.val());
+            res.json(snapshot.val());
+            userReference.off("value");
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+            res.send("The read failed: " + errorObject.code);
+        });
+    }))
 }
